175b533beeed93dc9c53dd3284e30754f164f1ca
el-dasher <el-dasher@users.noreply.github.com>
2020-11-21 23:03:13 -0400

    é gif, não png, BURRO

--- src/lib/db/data/assets/gifs/listener.py
+++ src/lib/db/data/assets/gifs/listener.py
@@ -1,14 +1,17 @@
 import os
 
 
-def png_listener(listdir: list) -> list:
+def gif_listener(listdir: list) -> list:
     for file in listdir[:]:
-        if not (file.endswith(".png")):
+        if not (file.endswith(".gif")):
             listdir.remove(file)
+
     return listdir
 
 
 def hug_gifs():
     filelist = os.listdir('./hug/')
-    png_listener(filelist)
+    gif_listener(filelist)
+
     return filelist
+
