db72e4bd9daa12b90f7083ec894ea6773335a328
el-dasher <el-dasher@users.noreply.github.com>
2020-11-24 18:35:08 -0400

    Update db.py

--- src/lib/db/db.py
+++ src/lib/db/db.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 DB_PATH = Path(".src/lib/db/data/db/database.db").absolute()
 BUILD_PATH = Path("./data/db/build.sql")
+print(DB_PATH),
+print(BUILD_PATH)
 cxn = connect(DB_PATH, check_same_thread=False)
 cur = cxn.cursor()
 
