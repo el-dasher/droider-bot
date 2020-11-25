832336d292e4b0110dee7228fbc4932279c6cf41
el-dasher <el-dasher@users.noreply.github.com>
2020-11-24 20:16:04 -0400

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -10,7 +10,7 @@ env_path = Path('..') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 COGS = [path.split("\\")[-1][:-3] for path in glob("src/lib/cogs/*.py")]
-COGS = [Path(cog.replace("/", ".")).absolute() for cog in COGS]
+COGS = [str(Path(cog.replace("/", ".")).absolute()) for cog in COGS]
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 PREFIX = getenv("PREFIX")
