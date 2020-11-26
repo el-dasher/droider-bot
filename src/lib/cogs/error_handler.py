9387184c3dca63742e6713256ef54c6d5af65119
el-dasher <el-dasher@users.noreply.github.com>
2020-11-26 00:25:39 -0400

    K

--- src/lib/cogs/error_handler.py
+++ src/lib/cogs/error_handler.py
@@ -1,6 +1,5 @@
 # This thing isn't working i will fix it later
 from discord.ext import commands
-import src.settings as settings
 from src.lib.utils.basic_utils import ready_up_cog
 
 
@@ -11,17 +10,6 @@ class ErrorHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         ready_up_cog(self.bot, __name__)
-
-    @commands.Cog.listener()
-    async def on_error(self):
-        # if error == "on_command_error":
-        #   pass
-        await self.bot.stdout.send(
-            f"Algo de errado, não está certo, talvez você tente falar com o"
-            f" <@{settings.MSCOY['user_id']}>, e ver o que ele pode fazer"
-        )
-        print("Algo de errado não esta certo")
-        raise
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, exc):
