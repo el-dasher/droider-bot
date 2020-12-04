7da966306adde036eb3bde38d44366652830f659
el-dasher <74831622+el-dasher@users.noreply.github.com>
2020-12-04 19:56:33 -0400

    update droid_data_getter.py

--- src/lib/utils/droid_data_getter.py
+++ src/lib/utils/droid_data_getter.py
@@ -147,6 +147,7 @@ async def get_droid_data(user_id):
         data_dict = {"user_data": user_data, "beatmap_data": beatmap_dicts, "pp_data": ppcheck_data}
     except NameError:
         data_dict = {"user_data": user_data, "beatmap_data": beatmap_dicts, "pp_data": [{"s": "OFFLINE"}]}
+    print(data_dict)
     if pp_data != "offline":
         await save_droid_uid_data(user_id, data_dict)
     # return data_dicts
