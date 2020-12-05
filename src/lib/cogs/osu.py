5628620ea10939c035b7f9c61fe1cc36a4b0e4eb
el-dasher <74831622+el-dasher@users.noreply.github.com>
2020-12-05 13:28:39 -0400

    update osu.py

--- src/lib/cogs/osu.py
+++ src/lib/cogs/osu.py
@@ -132,7 +132,8 @@ class OsuGame(commands.Cog):
             except IndexError:
                 return await ctx.reply(f"Não foi possivel encontrar o usuário: {user}")
         else:
-            return await ctx.reply(
+            user = osu_api.get_user({"u": DATABASE.child("OSU_USERS").child(ctx.author.id).get().val()["user"]}[0])
+            await ctx.reply(
                 "Você não tem uma conta cadastrada, utilize `ms!osuset <user>`"
                 "ou informe qual usuario você quer pegar a play recente `ms!rs <user>`"
             )
