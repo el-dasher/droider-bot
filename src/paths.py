bf7560421322c45edd0c335835f6ed0ca922d77b
el-dasher <74831622+el-dasher@users.noreply.github.com>
2020-12-15 21:53:44 -0400

    update paths.py

--- src/paths.py
+++ src/paths.py
@@ -5,16 +5,16 @@ from pathlib import Path
 debug = False # debug = True SE VOCÊ FOR RODAR O PROJETO LOCALMENTE!!!
 
 if debug:
-print("O BOT ESTÁ RODANDO LOCALMENTE")
-
-COGS_PATH = Path('./lib/cogs')
-
-LUCKY_PATH = Path("./lib/db/data/json/lucky_responses.json")
-MONTHS_PATH = Path("./lib/db/data/json/months.json")
+    print("O BOT ESTÁ RODANDO LOCALMENTE")
+    
+    COGS_PATH = Path('./lib/cogs')
+    
+    LUCKY_PATH = Path("./lib/db/data/json/lucky_responses.json")
+    MONTHS_PATH = Path("./lib/db/data/json/months.json")
 else :
-print('O BOT ESTÁ RODANDO EM "PRODUÇÃO')
-
-COGS_PATH = Path('./src/lib/cogs')
-
-LUCKY_PATH = Path("./src/lib/db/data/json/lucky_responses.json")
-MONTHS_PATH = Path("./src/lib/db/data/json/months.json")
+    print('O BOT ESTÁ RODANDO EM "PRODUÇÃO')
+    
+    COGS_PATH = Path('./src/lib/cogs')
+    
+    LUCKY_PATH = Path("./src/lib/db/data/json/lucky_responses.json")
+    MONTHS_PATH = Path("./src/lib/db/data/json/months.json")
