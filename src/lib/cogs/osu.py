3194dde68451d2fca109706530962c2a13ee1d02
el-dasher <74831622+el-dasher@users.noreply.github.com>
2020-12-19 07:43:46 -0400

    update osu.py

--- src/lib/cogs/osu.py
+++ src/lib/cogs/osu.py
@@ -418,23 +418,10 @@ class OsuDroid(commands.Cog):
                     for _, play in enumerate(user_data["pp_data"][start:end]):
                         index += 1
                         play["mods"] = _is_nomod(play["mods"])
-                        try:
-                            play["beatmap_data"] = \
-                                (await get_beatmap_data(play["hash"]))
-                            if "DT" in play["mods"] or "NC" in play["mods"]:
-                                play["beatmap_data"]["bpm"] = int(play["beatmap_data"]["bpm"]) * 1.50
-                        except IndexError:
-                            play["beatmap_data"] = {
-                                "max_combo": "0",
-                                "diff_aim": "0",
-                                "diff_speed": "0",
-                                "bpm": "0",
-                                "difficultyrating": "0",
-
-                                "beatmap_id": "0",
-                                "beatmapset_id": "0",
-                                "title": "?"
-                            }
+                        play["beatmap_data"] = await get_beatmap_data(play["hash"]))
+                        if "DT" in play["mods"] or "NC" in play["mods"]:
+                            play["beatmap_data"]["bpm"] = int(play["beatmap_data"]["bpm"]) * 1.50
+                        
                         user_data["pp_data"][_]['beatmap_data'] = play["beatmap_data"]
 
                         next_ppcheck_embed.add_field(
