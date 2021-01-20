ac0c085b17f9e2f439e6f924947136e715e43dc0
el-dasher <74831622+el-dasher@users.noreply.github.com>
2021-01-20 19:26:40 -0400

    Update droid_data_getter.py

--- src/lib/utils/osu/osu_droid/droid_data_getter.py
+++ src/lib/utils/osu/osu_droid/droid_data_getter.py
@@ -201,7 +201,7 @@ class OsuDroidProfile:
         recent_play = {}
         status_code = 200
         try:
-            recent_play = self.get_play_data(self._player_html.text.find("li", class_="list-group-item"))
+            recent_play = self.get_play_data(self._player_html.find("li", class_="list-group-item"))
         except (IndexError, KeyError, AttributeError):
             status_code = 400
         else:
